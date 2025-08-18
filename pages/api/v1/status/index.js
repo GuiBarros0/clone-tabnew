b4a2561bfb5ebef4251682cd83678b9425eaf0bb
Guilherme Barros <86804512+GuiBarros0@users.noreply.github.com>
2025-08-18 23:29:25 +0000

    refactor: config ssl `database.js` and refactor challenger data bd

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,6 +3,8 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updateAt = new Date().toISOString();
 
+  await database.query("SHOW server_version");
+
   const databaseVersionResult = await database.query("SHOW server_version");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
